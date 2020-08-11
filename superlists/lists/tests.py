8a8da04e5a589de8fda3c2355e59f82dc4d53b2b
Andrii Lavrov <faststep15@gmail.com>
2020-08-11 15:54:19 +0300

    refactoring tests

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,10 +1,4 @@
-from django.urls import resolve
 from django.test import TestCase
-from django.http import HttpRequest
-# from django.test import Client
-from django.template.loader import render_to_string
-
-from lists.views import home_page
 
 
 class HomePageTest(TestCase):
